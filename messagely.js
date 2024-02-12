f66a0bd8ab1b597fb176be95a1c835b2ffb6280b
lalaycorea <133055195+lalaycorea@users.noreply.github.com>
2024-02-12 16:31:32 -0500

    Update messagely.js

--- messagely.js
+++ messagely.js
@@ -1,4 +1,4 @@
-/** Express app for message.ly. */
+
 
 
 const express = require("express");
@@ -8,17 +8,16 @@ const { authenticateJWT } = require("./middleware/auth");
 const ExpressError = require("./expressError")
 const app = express();
 
-// allow both form-encoded and json body parsing
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// allow connections to all routes from any browser
 app.use(cors());
 
-// get auth token for all routes
+
 app.use(authenticateJWT);
 
-/** routes */
+
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
@@ -28,14 +27,13 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/messages", messageRoutes);
 
-/** 404 handler */
 
 app.use(function(req, res, next) {
   const err = new ExpressError("Not Found", 404);
   return next(err);
 });
 
-/** general error handler */
+
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
