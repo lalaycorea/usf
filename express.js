42e7ee5215a7de492a30790eae5388c88634afe7
lalaycorea <133055195+lalaycorea@users.noreply.github.com>
2024-02-12 15:27:40 -0500

    Update express.js

--- express.js
+++ express.js
@@ -1,7 +1,4 @@
-/**
- * Build a frequency counter object from an array
- * @param {Array} arr any array
- */
+
 function createFrequencyCounter(arr) {
   return arr.reduce(function(acc, next) {
     acc[next] = (acc[next] || 0) + 1;
@@ -9,10 +6,7 @@ function createFrequencyCounter(arr) {
   }, {});
 }
 
-/**
- * Find the most common element in the array
- * @param {Array} arr any array
- */
+
 function findMode(arr) {
   let freqCounter = createFrequencyCounter(arr);
 
@@ -29,11 +23,7 @@ function findMode(arr) {
   return +mostFrequent;
 }
 
-/**
- * Attempt to convert an array of strings to an array of numbers
- * @param {Array} numsAsStrings array of strings
- * @returns {Array|Error} an array or an error object
- */
+
 function convertAndValidateNumsArray(numsAsStrings) {
   let result = [];
 
