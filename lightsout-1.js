d99a6b86c0b575c00748f9d0d862d8650e547291
lalaycorea <133055195+lalaycorea@users.noreply.github.com>
2024-02-12 21:19:38 -0500

    Create lightsout-1.js

---
+++ lightsout-1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Cell.css";
+
+
+
+function Cell({ flipCellsAroundMe, isLit = false }) {
+  const classes = `Cell ${isLit ? "Cell-lit" : ""}`;
+  return <td className={classes} onClick={flipCellsAroundMe} role="button" />;
+}
+
+export default Cell;
